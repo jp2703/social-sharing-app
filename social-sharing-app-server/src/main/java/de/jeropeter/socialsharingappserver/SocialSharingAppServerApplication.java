31233063ede24276ece8f7b116452225cd723154
Jero Peter <j.peter@micromata.de>
2021-10-22 22:51:39 +0200

    initialized project social-sharing-app

---
+++ social-sharing-app-server/src/main/java/de/jeropeter/socialsharingappserver/SocialSharingAppServerApplication.java
@@ -0,0 +1,13 @@
+package de.jeropeter.socialsharingappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSharingAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSharingAppServerApplication.class, args);
+	}
+
+}
---
+++ social-sharing-app-server/src/test/java/de/jeropeter/socialsharingappserver/SocialSharingAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jeropeter.socialsharingappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSharingAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
